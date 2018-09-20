ab25a912c2ec2d47ad43a57ba3f87de7ca09190a
Tanvi Gangadhar <u1205740@utah.edu>
2018-09-20 11:34:00 -0600

    All tests passing

--- src/java/simpledb/HeapFileIterator.java
+++ src/java/simpledb/HeapFileIterator.java
@@ -1,47 +1,6 @@
 package simpledb;
 
 import java.util.Iterator;
-import java.util.NoSuchElementException;
-
-//public class HeapFileIterator extends AbstractDbFileIterator{
-//
-//	@Override
-//	public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
-//		// TODO Auto-generated method stub
-//		return super.next();
-//	}
-//	
-//	@Override
-//	public boolean hasNext() throws DbException, TransactionAbortedException {
-//		// TODO Auto-generated method stub
-//		return super.hasNext();
-//	}
-//	
-//	@Override
-//	public void open() throws DbException, TransactionAbortedException {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public void rewind() throws DbException, TransactionAbortedException {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//	
-//	@Override
-//	public void close() {
-//		// TODO Auto-generated method stub
-//		super.close();
-//	}
-//
-//	@Override
-//	protected Tuple readNext() throws DbException, TransactionAbortedException {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//}
-
 
 public class HeapFileIterator extends AbstractDbFileIterator {
 
@@ -80,14 +39,15 @@ public class HeapFileIterator extends AbstractDbFileIterator {
         // if the iterator hasnt' finished
         if (tupleIterator != null && tupleIterator.hasNext())
             return tupleIterator.next();
+
         //if there are more pages in the same file
-        if (pageId < heapFile.numPages()) {
+        if (pageId >= 0 && pageId < heapFile.numPages()) {
             HeapPageId pid = new HeapPageId(heapFile.getId(), pageId++);
             HeapPage heapPage = (HeapPage) Database.getBufferPool().getPage(this.tid, pid, Permissions.READ_ONLY);
             tupleIterator = heapPage.iterator();
-            //have to fix return statement
             return readNext();
         }
+
         //reached end of file
         else
             return null;
